1230435ea1d4de1fc332974ff2aed3daf732bee5
sabita <sabitakhadka091@gmail.com>
2023-09-07 16:21:35 +0545

    updating

--- TEXTUTILS/src/components/Textform.js
+++ TEXTUTILS/src/components/Textform.js
@@ -2,7 +2,7 @@ import React, {useState}  from 'react'
 
 export default function Textform(props) {
     const handleUpClick = () => {
-        console.log("Uppercase was clicked ")
+        console.log("Uppercase was clicked " ,text)
         let newtext = text.toUpperCase();
         setText(newtext)
     }
@@ -16,7 +16,7 @@ setText(event.target.value)
 return (
   <div>
     <h1>{props.heading}</h1>
-  <div className="mb-3">
+  <div className="mb-4">
    
     <textarea className="form-control" onChange={handleonChange} value={text} id="boxform" rows="8"></textarea>
     <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
