4f6b22cac54f6f8b4c54f738b9588756b1cedf11
sabita <sabitakhadka091@gmail.com>
2023-09-12 18:18:46 +0545

    updated

--- TEXTUTILS/src/App.js
+++ TEXTUTILS/src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Textform from "./components/Textform";
+// import Textform from "./components/Textform";
+import About from "./components/About";
 function App() {
+
   return (
     <>
       <Navbar title="textutils1 " Hometext="Home " />
       {/* <Navbar/> */}
-
       <div className="textform my-3">
-
-
-      <Textform id="box" heading = "Enter the text to analyse below"/>
-        </div>
+        {/* <Textform id="box" heading = "Enter the text to analyse below"/> */}
+      </div>
+      <div>
+        <About/>
+          
+      </div>
     </>
   );
 }
--- TEXTUTILS/src/components/Textform.js
+++ TEXTUTILS/src/components/Textform.js
@@ -1,6 +1,9 @@
 import React, {useState}  from 'react'
 import './Textform.css'
 export default function Textform(props) {
+  const [text, setText] = useState(" ")
+    // text = "enter the text"; // wrong way to change the state
+    // setText("Enter your opinion here");// correct way to change the state
     const handleUpClick = () => {
         console.log("Uppercase was clicked " ,text)
         let newtext = text.toUpperCase();
@@ -19,19 +22,21 @@ export default function Textform(props) {
 console.log("handleonChange")
 setText(event.target.value)
     }
-    const [text, setText] = useState(" ")
-    // text = "enter the text"; // wrong way to change the state
-    // setText("Enter your opinion here");// correct way to change the state
+  
 return (
   <>
   <div>
     <h1>{props.heading}</h1>
   <div className="mb-3">
    
-    <textarea className="form-control" onChange={handleonChange} value={text} id="boxform" rows="8"></textarea>
-    <button className="btn btn-warning mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-    <button className="btn btn-warning" onClick={handleloClick}>Convert to LowerCase</button>
-    <button className="btn btn-warning" onClick={handleclearClick}>Clear text</button>
+    <textarea  style={{
+      backgroundColor:'black',color:'white',fontSize:'2rem'
+    }}
+    className="form-control" onChange={handleonChange} value={text} id="boxform" rows="8"></textarea>
+    <button style={{margin:'2px',padding:'2px'}} className="btn btn-warning mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+    <button style={{margin:'2px',padding:'2px'}} className="btn btn-warning mx-1" onClick={handleloClick}>Convert to LowerCase</button>
+    <button style={{margin:'2px',padding:'2px'}} className="btn btn-warning mx-1" onClick={handleclearClick}>Clear text</button>
+
   </div>
   </div>
   <div className="container my-4">
